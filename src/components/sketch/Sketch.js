79069bf655afcbe9d4e65c77a1d6bea139bb4926
batitaki <taquinibautista@gmail.com>
2023-12-11 23:33:49 -0300

    mejorando sketch

--- src/components/sketch/Sketch.js
+++ src/components/sketch/Sketch.js
@@ -5,6 +5,8 @@ import videoFile from '../../assets/lacienaga.mp4';
 import audio from '../../assets/beat.wav';
 import audio1 from '../../assets/ojo.wav';
 import audio2 from '../../assets/soho.wav';
+import audio3 from '../../assets/rising.wav';
+import audio4 from '../../assets/space.wav';
 import suziImage from '../../assets/suzi.jpg';
 import valentinoImage from '../../assets/valen1.png';
 import jeeImage from '../../assets/jeee.png'
@@ -13,9 +15,17 @@ import './SketchStyles.css';
 let sound;
 let sound1;
 let sound2;
+let sound3;
+let sound4;
+let video;
+
 let isSoundOn = false;
 let isSound1On = false;
 let isSound2On = false;
+let isSound3On = false;
+let isSound4On = false;
+let isVideoPlaying = false;
+
 let suzi;
 let valentino;
 let jee;
@@ -24,8 +34,9 @@ let valentinoSize = 100;
 let jeeSize = 100;
 let angle = 0;
 let yOffset = 0;
-let video;
-let isVideoPlaying = false;
+
+let isHKeyPressed = false;
+let circleRadius = 0;
 
 export default (props) => {
   const x = 250;
@@ -35,6 +46,8 @@ export default (props) => {
     sound = p5.loadSound(audio);
     sound1 = p5.loadSound(audio1);
     sound2 = p5.loadSound(audio2);
+    sound3 = p5.loadSound(audio3);
+    sound4 = p5.loadSound(audio4);
     suzi = p5.loadImage(suziImage);
     valentino = p5.loadImage(valentinoImage);
     jee = p5.loadImage(jeeImage);
@@ -47,7 +60,7 @@ export default (props) => {
   };
 
   const draw = (p5) => {
-    p5.background(255, 400, 223);
+    p5.background(215, 300, 423);
 
     if (isSoundOn && sound.isPlaying()) {
       const scaleValue = p5.sin(angle);
@@ -103,6 +116,21 @@ export default (props) => {
       isSound2On = false;
     }
 
+    if (p5.keyIsDown(72) && !isSound3On) {
+      sound3.loop();
+      isSound3On = true;
+    } else if (!p5.keyIsDown(72) && isSound3On) {
+      sound3.stop();
+      isSound3On = false;
+    }
+
+    if (p5.keyIsDown(82) && !isSound4On) {
+      sound4.loop();
+      isSound4On = true;
+    } else if (!p5.keyIsDown(82) && isSound4On) {
+      sound4.stop();
+      isSound4On = false;
+    }
    
     if (p5.keyIsDown(86) && !isVideoPlaying) { 
       video.loop(); 
@@ -112,8 +140,27 @@ export default (props) => {
       isVideoPlaying = false;
     }
 
+    const videoWidth = 300; 
+    const videoHeight = (videoWidth / video.width) * video.height;
+  
     if (isVideoPlaying) {
-      p5.image(video, p5.width - video.width, 0);
+      p5.image(video, p5.width - videoWidth, 0, videoWidth, videoHeight);
+    }
+
+    if (p5.keyIsDown(72) && !isHKeyPressed) { 
+      isHKeyPressed = true;
+    } else if (!p5.keyIsDown(72) && isHKeyPressed) {
+      isHKeyPressed = false;
+      circleRadius = 0; 
+    }
+
+    if (isHKeyPressed) {
+      p5.fill(255, 0, 0);
+      p5.ellipse(p5.width / 2, p5.height / 2, circleRadius, circleRadius);
+      circleRadius += 5; 
+      if (circleRadius > 200) {
+        circleRadius = 200;
+      }
     }
   };
 
