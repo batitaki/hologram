d1dccb3fad2fd80a94357b524580340c20f31a68
¨batitaki¨ <taquinibautista@gmail.com>
2024-01-24 20:38:41 -0300

    pausa y despausa

--- src/components/sketch/DrawImagesComponent.js
+++ src/components/sketch/DrawImagesComponent.js
@@ -1,11 +1,15 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Sketch from 'react-p5';
 
 const DrawImagesComponent = () => {
   const [drawImage, setDrawImage] = useState(false);
   const [userImage, setUserImage] = useState(null);
+  const [shouldDraw, setShouldDraw] = useState(true);
+  const [isPaused, setIsPaused] = useState(
+    localStorage.getItem('isPaused') === 'true' ? true : false
+  );
   const imgRef = useRef(null);
-  const imagesHistory = useRef([]); // Almacena las imágenes anteriores
+  const imagesHistory = useRef([]);
 
   const handleImageUpload = (p5, e) => {
     const file = e.target.files[0];
@@ -16,6 +20,7 @@ const DrawImagesComponent = () => {
         const img = p5.loadImage(reader.result, () => {
           setUserImage(img);
           setDrawImage(true);
+          setShouldDraw(true);
         });
         imgRef.current = img;
       };
@@ -32,23 +37,30 @@ const DrawImagesComponent = () => {
   const draw = (p5) => {
     p5.background(255);
 
-    // Dibujar todas las imágenes almacenadas en el historial
     for (let i = 0; i < imagesHistory.current.length; i++) {
       const { img, x, y } = imagesHistory.current[i];
       const imgSize = 100;
       p5.image(img, x - imgSize / 2, y - imgSize / 2, imgSize, imgSize);
     }
 
-    if (drawImage && userImage) {
+    if (drawImage && userImage && shouldDraw && !isPaused) {
       const imgSize = 100;
       const currentImage = { img: userImage, x: p5.mouseX, y: p5.mouseY };
       imagesHistory.current.push(currentImage);
     }
   };
 
-  const mouseMoved = (p5) => {
-    // No es necesario para esta implementación, pero puedes dejarlo aquí si lo necesitas
+  const mousePressed = () => {
+    const newIsPaused = !isPaused;
+    setIsPaused(newIsPaused);
+    localStorage.setItem('isPaused', newIsPaused.toString());
   };
+
+  useEffect(() => {
+    if (imgRef.current) {
+      imgRef.current.pause = isPaused;
+    }
+  }, [isPaused]);
 
   const keyTyped = (p5) => {
     if (p5.key === 'U' || p5.key === 'u') {
@@ -65,7 +77,7 @@ const DrawImagesComponent = () => {
         setup={(p5, canvasParentRef) => setup(p5, canvasParentRef)}
         draw={(p5) => draw(p5)}
         keyTyped={(p5) => keyTyped(p5)}
-        mouseMoved={(p5) => mouseMoved(p5)}
+        mousePressed={() => mousePressed()}
       />
       <input
         type="file"
