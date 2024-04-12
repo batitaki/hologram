038125c16caac6b337aa9495483d5a34c1802336
batitaki <taquinibautista@gmail.com>
2024-04-12 00:09:37 -0300

    limpiando el codigo

--- src/components/sketch/audio/ParticleComponent.js
+++ src/components/sketch/audio/ParticleComponent.js
@@ -60,23 +60,18 @@ const ParticleComponent = () => {
   const draw = (p5) => {
     p5.background(0);
   
-    // Si el audio está reproduciéndose, dibujar las líneas
     if (audioPlaying) {
-      // Calcular el centro del canvas
+
       const centerX = p5.width / 2;
       const centerY = p5.height / 2;
-  
-      // Definir el número de líneas y el ángulo entre cada una
+
       const numLines = 300;
       const angleIncrement = p5.TWO_PI / numLines;
   
-      // Definir el máximo tamaño de las líneas
       const maxLength = p5.dist(0, 0, centerX, centerY);
-  
-      // Calcular la longitud actual de las líneas
+
       let lineLength = (sound.currentTime / sound.duration) * maxLength;
   
-      // Dibujar líneas rojas desde todos los lados hacia el centro
       p5.stroke(255, 0, 0);
       p5.strokeWeight(2);
       for (let i = 0; i < numLines; i++) {
@@ -88,27 +83,24 @@ const ParticleComponent = () => {
     }
   
     for (const particle of particles) {
-      // Calcular la dirección hacia la que se moverá la partícula
+
       const dx = p5.mouseX - particle.x;
       const dy = p5.mouseY - particle.y;
       const distance = p5.dist(p5.mouseX, p5.mouseY, particle.x, particle.y);
       const directionX = dx / distance;
       const directionY = dy / distance;
   
-      // Mover la partícula hacia el cursor
       particle.x += directionX * particle.speed;
       particle.y += directionY * particle.speed;
-  
-      // Dibujar la partícula
+
       p5.stroke(255, 0, 0, particle.opacity);
       p5.fill(255, 0, 0, particle.opacity);
       p5.ellipse(particle.x, particle.y, particle.size, particle.size);
     }
   
-    // Reproducir audio cuando todas las partículas estén cerca del cursor
     const allParticlesClose = particles.every((particle) => {
       const distance = p5.dist(p5.mouseX, p5.mouseY, particle.x, particle.y);
-      return distance < 50; // Ajusta el valor de 50 según la proximidad deseada
+      return distance < 50; 
     });
     if (allParticlesClose) {
       playAudio();
